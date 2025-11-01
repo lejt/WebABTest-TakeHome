68142d0ea09117887968a292b900fda543b938fe
lejt <92820402+lejt@users.noreply.github.com>
2025-11-01 15:08:41 -0700

    update task1: use hubspot parent without dup

--- task1.js
+++ task1.js
@@ -1,246 +1,280 @@
-// hard to target hubspot elements, it needs to render first, it also is rendered async, cloning /removing those elements will strip associations
-// can only reference or manipulate them in place
-
-// Element identifiers
-const FORM_FLEX_PARENT = '.contact-form__inner';
-const FORM_SELECTOR = '.contact-form__form';
-const FORM_FIRST_LAST_NAME = '.form-columns-2';
-const FORM_FIRST_NAME = '.hs_firstname';
-const FORM_LAST_NAME = '.hs_lastname';
-const FORM_EMAIL = '.hs_email';
-const FORM_HELP_INPUT = '.hs_how_can_we_help_you___contact_us_form_';
-const FORM_CONSENT = '.legal-consent-container';
-const FORM_SUBMIT = '.hs_submit';
+// constants
+const FLEX_CONTAINER = '.contact-form__inner';
+const FORM_PARENT = '.contact-form__form';
+const FORM_SELECTOR = '.hbspt-form';
+const FORM_STEP_1_FIELDS = '.form-columns-2, .hs_email';
+const FORM_STEP_2_FIELDS =
+  '.hs_how_can_we_help_you___contact_us_form_, .legal-consent-container, .hs_submit';
+const FORM_OMITTED =
+  '.hs_conversion__how_did_you_hear_about_us_, .hs_recaptcha';
+const FORM_ERROR_MESSAGES = '.hs-error-msgs';
 const INJECTED_HEADER_TEXT = 'Hello Conversion!';
 const INJECTED_PARAGRAPH_TEXT = 'Click on the button below to contact us.';
 const INJECTED_BUTTON_TEXT = 'Click Here';
-
-// Elements
-const formFlexParent = document.querySelector(FORM_FLEX_PARENT);
-const originalFormElement = document.querySelector(FORM_SELECTOR);
-const originalFormContent = originalFormElement.querySelector('form');
-
-const originalFormFirstLastNames = document.querySelector(FORM_FIRST_LAST_NAME);
-
-const originalFormFirstName = document.querySelector(FORM_FIRST_NAME);
-const originalFormLastName = document.querySelector(FORM_LAST_NAME);
-const originalFormEmail = document.querySelector(FORM_EMAIL);
-const originalFormHelpInput = document.querySelector(FORM_HELP_INPUT);
-const originalFormConsent = document.querySelector(FORM_CONSENT);
-const originalFormSubmitButton = document.querySelector(FORM_SUBMIT);
-
-const overlay = document.createElement('div');
-const injectedContent = document.createElement('div');
-const injectedHeader = document.createElement('h3');
-const injectedParagraph = document.createElement('p');
-const injectedButtonParent = document.createElement('div');
-const injectedButton = document.createElement('button');
-
-const injectedModal = document.createElement('dialog');
-const modalHeader = document.createElement('div');
-const modalBody = document.createElement('div');
-const modalFooter = document.createElement('div');
-const modalStep1 = document.createElement('div');
-const modalStep2 = document.createElement('div');
-const modalStep3 = document.createElement('div');
-const modalProgressBar = document.createElement('div');
-const modalThankYouMessage = document.createElement('h4');
-const modalNextButton = document.createElement('button');
-const modalBackButton = document.createElement('button');
-const modalCloseButton = document.createElement('button');
+const MODAL_CLOSE = 'X';
+const MODAL_NEXT = 'Next ->';
+const MODAL_BACK = '<- Back';
+const MODAL_SUCCESS = 'You have submitted your information successfully.';
 
 const style = document.createElement('style');
-
-overlay.setAttribute('data-ab-element-id', 'injected-overlay');
-injectedContent.setAttribute('data-ab-element-id', 'injected-content');
-injectedButtonParent.setAttribute('data-ab-element-id', 'button-wrapper');
-injectedButton.setAttribute('data-ab-element-id', 'open-modal-btn');
-injectedParagraph.setAttribute('data-ab-element-id', 'injected-paragraph');
-injectedModal.setAttribute('data-ab-element-id', 'injected-modal');
-modalHeader.setAttribute('data-ab-element-id', 'modal-header');
-modalBody.setAttribute('data-ab-element-id', 'modal-body');
-modalFooter.setAttribute('data-ab-element-id', 'modal-footer');
-modalStep1.setAttribute('data-ab-step', '1');
-modalStep2.setAttribute('data-ab-step', '2');
-modalStep3.setAttribute('data-ab-step', '3');
-modalProgressBar.setAttribute('data-ab-element-id', 'modal-progress-bar');
-modalNextButton.setAttribute('data-ab-element-id', 'modal-next-button');
-modalBackButton.setAttribute('data-ab-element-id', 'modal-back-button');
-modalCloseButton.setAttribute('data-ab-element-id', 'modal-close-button');
-
-const bgColor = window.getComputedStyle(originalFormElement).backgroundColor;
-const buttonBgColor = window.getComputedStyle(
-  originalFormSubmitButton
-).backgroundColor;
-
-injectedHeader.textContent = INJECTED_HEADER_TEXT;
-injectedParagraph.textContent = INJECTED_PARAGRAPH_TEXT;
-injectedButton.textContent = INJECTED_BUTTON_TEXT;
-modalCloseButton.textContent = 'x';
-modalBackButton.textContent = '<-';
-modalNextButton.textContent = '->';
-
 style.textContent = `
-  ${FORM_FLEX_PARENT} {
-    align-items: flex-start;
-  }
-
-  [data-ab-element-id="injected-overlay"] {
-    position: fixed;
-    inset: 0;
-    background: rgba(0,0,0,0.5);
-    z-index: 99999;
-  }
-
-  ${FORM_SELECTOR} {
-    position: relative;
-    background: ${bgColor};
-    color: #000;
-    z-index: 100000;
-  }
-
-  [data-ab-element-id="injected-content"] {
-    display: flex;
-    flex-direction: column; 
-  }
-
-  [data-ab-element-id="button-wrapper"] {
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  [data-ab-element-id="open-modal-btn"] {
-    height: 40px;
-    width: 100px;
-    background: #5E5D5D;
-    cursor: pointer;
-
-    :hover {
+    ${FLEX_CONTAINER} {
+      align-items: flex-start;
     }
-  }
-    
-  [data-ab-element-id="button-wrapper"] {
-    display: flex;
-    justify-content: flex-end;
-  }
-    
-  [data-ab-element-id="injected-paragraph"] {
-    color: dark slate gray;
-  }
+    [data-ab-element-id="injected-overlay"] {
+      position: fixed;
+      inset: 0;
+      background: rgba(0,0,0,0.5);
+      z-index: 99999;
+    }
+    [data-ab-element-id="form-container"] {
+      background: #fff;
+      z-index: 100000; 
+    }
 
-  [data-ab-element-id="modal-body"] {
-    display: flex;
-    flex-direction: column;
-  }
+    [data-ab-element-id="injected-paragraph"] {
+      color: darkgray;
+    }
+    [data-ab-element-id="injected-button-wrapper"] {
+      display: flex;
+      justify-content: flex-end;
+    }
+    [data-ab-element-id="open-modal-btn"] {
+      background: #5E5D5D;
+      color: white;
+      border: 2px solid #5E5D5D;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+    [data-ab-element-id="open-modal-btn"]:hover {
+      background: #333;
+      border: 2px solid gray;
+    }
 
-  [data-ab-element-id="modal-header"], [data-ab-element-id="modal-footer"] {
-    display: flex;
-    justify-content: flex-end;
-  }
+    [data-ab-element-id="form-modal"] {
+      border: none;
+      border-radius: 6px;
+      padding: 1.5rem;
+      width: 500px;
+      max-width: 90%;
+    }
 
-  [data-ab-element-id="modal-close-button"] {
-    cursor: pointer;
-  }
+    [data-ab-element-id="form-modal"] button {
+      margin: 2px;
+    }
 
-  [data-ab-element-id="modal-close-button"]:hover {
-    color: gray;
-  }
+    [data-ab-element-id="modal-header"], [data-ab-element-id="modal-footer"] {
+      display: flex;
+      justify-content: flex-end;
+    }
 
-  [data-ab-element-id="modal-next-button"], [data-ab-element-id="modal-back-button"] {
-    width: 40px;
-    height: 20px;
-    border: 2px solid black;
-    cursor: pointer;
-  }
+    [data-ab-element-id="modal-next"], [data-ab-element-id="modal-back"] {
+      border: 2px solid black;
+      cursor: pointer;
+    }
 
-  [data-ab-element-id="modal-next-button"]:disabled, [data-ab-element-id="modal-back-button"]:disabled {
-    background: gray;
-    color: #fff;
-    opacity: 0.6;
-    cursor: not-allowed;
-    border-color: #999;
-  }
+    [data-ab-element-id="modal-next"]:disabled, [data-ab-element-id="modal-back"]:disabled {
+      background: gray;
+      color: #fff;
+      opacity: 0.6;
+      cursor: not-allowed;
+      border-color: #999;
+    }
 
-  [data-ab-element-id="modal-next-button"]:not(:disabled):hover,
-  [data-ab-element-id="modal-back-button"]:not(:disabled):hover {
-    background: #444;
-    color: white;
-  }
-`;
+    [data-ab-element-id="modal-next"]:not(:disabled):hover,
+    [data-ab-element-id="modal-back"]:not(:disabled):hover {
+      background: #444;
+      color: white;
+    }
 
-// initial modal state
-let modalCurrentStep = 1;
-const modalStates = { 1: modalStep1, 2: modalStep2, 3: modalStep3 };
+    [data-ab-element-id="modal-step"] {
+      display: none;
+    }
+    [data-ab-element-id="modal-step"].active {
+      display: block;
+    }
 
-document.addEventListener('click', (e) => {
-  const clickedInside = originalFormElement.contains(e.target);
-  if (!clickedInside) {
-    overlay.remove();
-  }
-});
-overlay.addEventListener('click', (e) => {
-  if (!originalFormElement.contains(e.target)) {
-    overlay.remove();
-  }
-});
-originalFormElement.addEventListener('click', (e) => e.stopPropagation());
-injectedButton.addEventListener('click', function () {
-  injectedModal.showModal();
-});
+    .modal-progress {
+      text-align: center;
+      margin-bottom: 1rem;
+      font-weight: bold;
+    }
 
-// modal actions
-modalCloseButton.addEventListener('click', () => injectedModal.close());
-modalNextButton.addEventListener('click', () => {
-  // validate form is filled correctly first
-  modalCurrentStep++;
-  updateStepDisplay();
-});
-modalBackButton.addEventListener('click', () => {
-  modalCurrentStep--;
-  updateStepDisplay();
-});
+  `;
 
-const updateStepDisplay = () => {
-  const currentStepAsStr = String(modalCurrentStep);
-  Object.keys(modalStates).forEach((key) => {
-    modalStates[key].hidden = key !== currentStepAsStr;
-  });
-  modalNextButton.disabled = modalCurrentStep === 3;
-  modalBackButton.disabled = modalCurrentStep === 1;
+// Wait for async HubSpot form to render
+const waitForHubspotForm = setInterval(() => {
+  const form = document.querySelector(FORM_SELECTOR);
+  if (!form) return;
+
+  clearInterval(waitForHubspotForm);
+  initABExperiment(form);
+}, 300);
+
+const createLandingContent = () => {
+  const injectedWrapper = document.createElement('div');
+  const injectedHeader = document.createElement('h3');
+  const injectedParagraph = document.createElement('p');
+  const injectedButtonWrapper = document.createElement('div');
+  const injectedButton = document.createElement('button');
+
+  injectedParagraph.setAttribute('data-ab-element-id', 'injected-paragraph');
+  injectedButtonWrapper.setAttribute(
+    'data-ab-element-id',
+    'injected-button-wrapper'
+  );
+  injectedButton.setAttribute('data-ab-element-id', 'open-modal-btn');
+  injectedHeader.textContent = INJECTED_HEADER_TEXT;
+  injectedParagraph.textContent = INJECTED_PARAGRAPH_TEXT;
+  injectedButton.textContent = INJECTED_BUTTON_TEXT;
+
+  injectedButtonWrapper.appendChild(injectedButton);
+  injectedWrapper.append(
+    injectedHeader,
+    injectedParagraph,
+    injectedButtonWrapper
+  );
+
+  return { injectedWrapper, injectedButton };
 };
 
-// Hide original content
-originalFormContent.hidden = true;
-modalStep2.hidden = true;
-modalStep3.hidden = true;
-updateStepDisplay();
+const createModal = () => {
+  const modal = document.createElement('dialog');
+  const modalHeader = document.createElement('div');
+  const modalBody = document.createElement('div');
+  const modalFooter = document.createElement('div');
+  const modalClose = document.createElement('button');
 
-// Inject elements
-document.body.appendChild(overlay);
-document.head.appendChild(style);
-injectedButtonParent.appendChild(injectedButton);
-injectedContent.append(
-  injectedHeader,
-  injectedParagraph,
-  injectedButtonParent,
-  injectedModal
-);
-originalFormElement.appendChild(injectedContent);
+  modal.setAttribute('data-ab-element-id', 'form-modal');
+  modalHeader.setAttribute('data-ab-element-id', 'modal-header');
+  modalBody.setAttribute('data-ab-element-id', 'modal-body');
+  modalFooter.setAttribute('data-ab-element-id', 'modal-footer');
 
-// Modal
-modalStep1.append(
-  // originalFormFirstName,
-  // originalFormLastName,
-  originalFormFirstLastNames,
-  originalFormEmail
-);
-modalStep2.append(
-  originalFormHelpInput,
-  originalFormConsent,
-  originalFormSubmitButton
-);
-modalStep3.append(modalThankYouMessage);
-modalHeader.appendChild(modalCloseButton); // ADD failure message?
-modalBody.append(modalProgressBar, modalStep1, modalStep2, modalStep3);
-modalFooter.append(modalBackButton, modalNextButton);
-injectedModal.append(modalHeader, modalBody, modalFooter);
+  modalClose.textContent = MODAL_CLOSE;
+
+  modalHeader.appendChild(modalClose);
+  modal.append(modalHeader, modalBody, modalFooter);
+
+  modalClose.addEventListener('click', () => modal.close());
+
+  return { modal, modalHeader, modalBody, modalFooter };
+};
+
+const initABExperiment = (form) => {
+  // hide original form
+  form.style.display = 'none';
+
+  // Hubspot form elements
+  const hubSpotForm = form;
+  hubSpotForm.style.display = 'block'; // must display for Hubspot children to be rendered
+  const modalStep1Fields = hubSpotForm.querySelectorAll(FORM_STEP_1_FIELDS);
+  const modalStep2Fields = hubSpotForm.querySelectorAll(FORM_STEP_2_FIELDS);
+  const modalOmittedFields = hubSpotForm.querySelectorAll(FORM_OMITTED);
+  const modalErrorMessages = hubSpotForm.querySelectorAll(FORM_ERROR_MESSAGES);
+
+  const { injectedWrapper, injectedButton } = createLandingContent();
+  const { modal, modalBody, modalFooter } = createModal();
+
+  const modalProgress = document.createElement('div');
+  const modalNext = document.createElement('button');
+  const modalBack = document.createElement('button');
+  const modalStep1 = document.createElement('div');
+  const modalStep2 = document.createElement('div');
+  const modalStep3 = document.createElement('div');
+  modalProgress.setAttribute('data-ab-element-id', 'modal-progress');
+  modalBack.setAttribute('data-ab-element-id', 'modal-back');
+  modalNext.setAttribute('data-ab-element-id', 'modal-next');
+  modalStep1.setAttribute('data-ab-element-id', 'modal-step');
+  modalStep2.setAttribute('data-ab-element-id', 'modal-step');
+  modalStep1.classList.add('active');
+
+  modalNext.textContent = MODAL_NEXT;
+  modalBack.textContent = MODAL_BACK;
+  modalStep3.textContent = MODAL_SUCCESS;
+
+  const formContainer = document.querySelector(FORM_PARENT);
+  formContainer.setAttribute('data-ab-element-id', 'form-container');
+  const overlay = document.createElement('div');
+  overlay.setAttribute('data-ab-element-id', 'injected-overlay');
+
+  // event listeners
+  let modalCurrentStep = 1;
+  const modalSteps = [modalStep1Fields, modalStep2Fields];
+
+  injectedButton.addEventListener('click', () => modal.showModal());
+
+  document.addEventListener('click', (e) => {
+    const clickedInsideInjectedWrapper = formContainer.contains(e.target);
+    if (!clickedInsideInjectedWrapper) {
+      overlay.remove();
+    }
+  });
+  overlay.addEventListener('click', (e) => {
+    if (!injectedWrapper.contains(e.target)) {
+      overlay.remove();
+    }
+  });
+
+  modalNext.addEventListener('click', () => handleStepChange(1));
+  modalBack.addEventListener('click', () => handleStepChange(-1));
+
+  const handleStepChange = (increment) => {
+    const currentFields = modalSteps[modalCurrentStep - 1];
+
+    // check for error messages
+    const hasVisibleErrors = Array.from(
+      document.querySelectorAll(FORM_ERROR_MESSAGES)
+    ).some((err) => err.offsetParent !== null);
+
+    // check if inputs filled
+    const allFilled = Array.from(currentFields)
+      .flatMap((field) =>
+        Array.from(field.querySelectorAll('input, textarea, select'))
+      )
+      .filter((el) => el.offsetParent !== null)
+      .every((input) => input.value.trim() !== '');
+
+    if (hasVisibleErrors || !allFilled) {
+      console.warn('Please fill all required fields before continuing.');
+      return;
+    }
+
+    const newStep = modalCurrentStep + increment;
+    if (newStep >= 1 && newStep <= modalSteps.length) {
+      modalCurrentStep = newStep;
+      showSteps();
+    }
+  };
+
+  const showSteps = () => {
+    modalNext.disabled = modalCurrentStep === modalSteps.length;
+    modalBack.disabled = modalCurrentStep === 1;
+    modalProgress.textContent = `Step ${modalCurrentStep} of ${modalSteps.length}`;
+
+    [...modalStep1Fields, ...modalStep2Fields, ...modalOmittedFields].forEach(
+      (field) => {
+        field.style.display = 'none';
+      }
+    );
+
+    const currentStepFields = modalSteps[modalCurrentStep - 1];
+    currentStepFields.forEach((field) => {
+      field.style.display = 'block';
+    });
+  };
+  showSteps();
+
+  // display on page
+  document.head.appendChild(style);
+  formContainer.append(injectedWrapper, modal);
+  document.body.appendChild(overlay);
+
+  modalBody.append(modalProgress, modalStep1, modalStep2);
+  modalStep1.appendChild(hubSpotForm);
+  modalFooter.append(modalBack, modalNext);
+};
+
+// NEXT STEPS
+// 2) progress bar
+// 3) success message
+// 4) deal with flicker
